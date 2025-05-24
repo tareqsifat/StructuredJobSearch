c582fe5fe761a4348315a994bdcf25c39c1ebbd1
unknown <mdtareq.me@gmail.com>
2025-05-24 19:30:52 +0600

    upload.php updated

--- upload.php
+++ upload.php
@@ -3,8 +3,20 @@
 use Sifat\WebScrapingCurl\UploadJobsController;
 
 require __DIR__ . '/vendor/autoload.php'; // Load Composer's autoloader
-
+$data = [
+    "type" => "govt",
+    "jobLink" => null,
+    "companyName" => "চট্টগ্রাম বন্দর কর্তৃপক্ষ",
+    "jobTitle" => "সহকারী প্রকৌশলী মেরিন",
+    "application_deadline" => null,
+    "is_pdf" => "false",
+    "image_link" => "http://imgs.bdjobs.com/scannedjobads/279706.jpg",
+    "pdf_link" => null,
+    "job_url" => "https://jobs.bdjobs.com/jobdetails.asp?id=279706&fcatId=-1&ln=2",
+    "job_source" => "bdjobs",
+    "id" => "279706"
+];
 
 $UploadJobsController = new UploadJobsController();
-$UploadJobsController->uploadJobs();
+$UploadJobsController->uploadJobs($data);
 echo "done";
